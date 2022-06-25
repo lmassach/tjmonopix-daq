78b520066fbb6577535c207d4d6b007c4a3d9bec
Ludovico Massaccesi <62757383+lmassach@users.noreply.github.com>
2022-06-25 16:39:48 +0200

    Windows dependencies for launch_and_plot

--- examples/launch_ana_and_plot.py
+++ examples/launch_ana_and_plot.py
@@ -1,5 +1,12 @@
 """Launches the analysis and plotting on new files as soon as ready.
 Must be run with Python3."""
+# **Dependencies on Windows**
+# For Phython3: must install numpy+mkl from <https://www.lfd.uci.edu/~gohlke/pythonlibs/>,
+# and then install tables (you may need to uninstall numexpr if you had already installed tables).
+#  - <https://download.lfd.uci.edu/pythonlibs/archived/cp37/numpy-1.21.6+mkl-cp37-cp37m-win_amd64.whl>
+#  - <https://download.visualstudio.microsoft.com/download/pr/6b6923b0-3045-4379-a96f-ef5506a65d5b/426A34C6F10EA8F7DA58A8C976B586AD84DD4BAB42A0CFDBE941F1763B7755E5/VC_redist.x64.exe>
+
+Complete list of dependencies: numpy scipy matplotlib tables
 import os
 import subprocess
 import argparse
