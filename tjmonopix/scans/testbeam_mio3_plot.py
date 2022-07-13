708fcba622873e58c24af55e0342e5d2578e3f19
Ludovico Massaccesi <62757383+lmassach@users.noreply.github.com>
2022-07-13 15:57:55 +0200

    Added ToT and hit delta-t plots

--- tjmonopix/scans/testbeam_mio3_plot.py
+++ tjmonopix/scans/testbeam_mio3_plot.py
@@ -25,11 +25,14 @@ if __name__ == "__main__":
     # Hitmap
     h, _, _ = np.histogram2d(tjm_hits["col"], tjm_hits["row"], bins=[112, 224],
                              range=[[0, 112], [0, 224]])
-    m = np.quantile(h.reshape(-1), 0.98)  # Set the maximum so that 98% of the pixels do not overflow
+    # Set the maximum so that 98% of the pixels do not overflow
+    hf = h.reshape(-1)
+    m = np.quantile(hf[hf>0], 0.98)
     m = np.ceil(m * 1.2)
+    # Color in red pixels that overflow
     cm = mpl.cm.get_cmap('viridis')
     cm.set_bad('#ff3333')
-    cm.set_over('#ff3333')  # Color in red pixels that overflow
+    cm.set_over('#ff3333')
     plt.hist2d(tjm_hits["col"], tjm_hits["row"], bins=[112, 224], range=[[0, 112], [0, 224]],
                vmin=0, vmax=m, cmap=cm)
     plt.colorbar().set_label("Number of hits (red for out of scale)")
@@ -37,3 +40,30 @@ if __name__ == "__main__":
     plt.ylabel("Row")
     plt.title("Hit map")
     plt.savefig(output_file + "_hitmap.png")
+    plt.clf()
+    # List pixels that overflow (may be useful for detecting noisy pixels)
+    print("List of pixels with > %d hits" % m)
+    print([(a, b) for a, b in np.argwhere(h > m)])
+
+    # ToT histogram
+    plt.hist((tjm_hits["te"] - tjm_hits["le"]) & 0x3f, bins=64, range=[0, 64])
+    plt.xlabel("ToT [25 ns]")
+    plt.ylabel("Hit count")
+    plt.title("ToT")
+    plt.grid(axis='y')
+    plt.savefig(output_file + "_tot.png")
+    plt.clf()
+
+    # Hit delta-t histogram
+    dt = np.diff(tjm_hits["timestamp"]) / 40
+    # Set the maximum to include 98% of the samples
+    m = np.quantile(dt, 0.98)
+    m = np.ceil(m * 1.2)
+    plt.hist(dt, bins=100, range=[0, m])
+    plt.xlabel("$\\Delta t$ between hits [$\\mu$s]")
+    plt.ylabel("Count")
+    plt.title("Interval between successive hits")
+    plt.grid(axis='y')
+    plt.yscale('log')
+    plt.savefig(output_file + "_hit-dt.png")
+    plt.clf()
